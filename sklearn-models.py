c2d5cb5e29d1565470248dd4f071666646b300a0
Bryant CH <91674676+Rexyonist@users.noreply.github.com>
2024-05-12 17:36:20 +0700

    update codebase

--- sklearn-models.py
+++ sklearn-models.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, ConfusionMatrixDisplay, confusion_matrix
 from sklearn.pipeline import Pipeline
 from textblob import TextBlob
 import joblib
+import matplotlib.pyplot as plt
 
 # Load the dataset
-df = pd.read_excel('output/excel-data.xlsx')
-df.to_csv
+df = pd.read_csv('output/labelled_data.csv')
 
 def assign_sentiment(text):
     analysis = TextBlob(text)
@@ -21,19 +21,34 @@ def assign_sentiment(text):
     else:
         return 'negative'
 
-df['sentiment'] = df['cleared_translate'].apply(assign_sentiment)
+# Hitung panjang teks
+df['text_length'] = df['stemmed'].apply(len)
 
-# Split the data into features (X) and cleared_translates (y)
-X = df['cleared_translate']
-y = df['sentiment']  # Ensure this column exists and contains correct target cleared_translates
+# Hitung batas atas dan bawah untuk panjang teks
+Q1 = df['text_length'].quantile(0.25)
+Q3 = df['text_length'].quantile(0.75)
+IQR = Q3 - Q1
+
+limit_under = Q1 - 1.5 * IQR
+limit_above = Q3 + 1.5 * IQR
+
+# Identifikasi outlier berdasarkan panjang teks
+outlier_idx = df[(df['text_length'] < limit_under) | (df['text_length'] > limit_above)].index
+
+# Hapus outlier
+df_cleaned = df.drop(outlier_idx)
+#df_cleaned['sentiment'] = df_cleaned['translated'].apply(assign_sentiment)
+
+# Sekarang gunakan df_cleaned untuk training model
+X_cleaned = df_cleaned['translated']
+y_cleaned = df_cleaned['sentiment']
+
 print("Setting up TfidfVectorizer and models...")
 
-# Define the preprocessing and models pipelines
 tfidf = TfidfVectorizer(stop_words='english', max_features=10000, ngram_range=(1, 2))
-nb_model = MultinomialNB()
+nb_model = BernoulliNB()
 svm_model = SVC()
 
-# Create a pipeline
 nb_pipeline = Pipeline([
     ('tfidf', tfidf),
     ('classifier', nb_model)
@@ -43,51 +58,53 @@ svm_pipeline = Pipeline([
     ('tfidf', tfidf),
     ('classifier', svm_model)
 ])
+
 print("Pipelines set up.")
 
-# Define the parameter grids for GridSearchCV
 print("Setting up parameter grids for grid search...")
 nb_param_grid = {
     'tfidf__max_features': [5000, 10000],
-    'classifier__alpha': [0.1, 1.0, 10.0]
+    'classifier__alpha': [0.1, 1, 10]
 }
 
 svm_param_grid = {
     'tfidf__max_features': [5000, 10000],
     'classifier__C': [0.1, 1, 10],
-    'classifier__kernel': ['linear', 'poly', 'rbf']
+    'classifier__gamma': ['scale', 'auto', 0.1, 1, 10],
 }
+
 print("Parameter grids set up.")
 
-# Perform GridSearchCV for each model
-cv = KFold(n_splits=5, shuffle=True, random_state=42)
-
+cv = KFold(n_splits=5, shuffle=True, random_state=52)
 nb_gs = GridSearchCV(nb_pipeline, param_grid=nb_param_grid, cv=cv, scoring='accuracy')
 svm_gs = GridSearchCV(svm_pipeline, param_grid=svm_param_grid, cv=cv, scoring='accuracy')
 
-# Fit the GridSearchCV models
-nb_gs.fit(X, y)
-svm_gs.fit(X, y)
+nb_gs.fit(X_cleaned, y_cleaned)
+svm_gs.fit(X_cleaned, y_cleaned)
 
 print("Getting the best models...")
-# Get the best model for each classifier
 best_nb_model = nb_gs.best_estimator_
 best_svm_model = svm_gs.best_estimator_
 
+
 print("Best models obtained.")
-# Evaluate the models on the testing set
 def evaluate_model(model, X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=62)
     model.fit(X_train, y_train)
     predictions = model.predict(X_test)
+    
     accuracy = accuracy_score(y_test, predictions)
     precision = precision_score(y_test, predictions, average='macro')
     recall = recall_score(y_test, predictions, average='macro')
+    
+    cm_display = ConfusionMatrixDisplay.from_predictions(y_test, predictions)
+    cm_display.ax_.set_title(f'Confusion Matrix for {type(model).__name__}')
+    #plt.show()
+    
     return accuracy, precision, recall
 
-# Evaluate best models
 for model_name, model in [('Naive Bayes', best_nb_model), ('SVM', best_svm_model)]:
-    accuracy, precision, recall = evaluate_model(model, X, y)
+    accuracy, precision, recall = evaluate_model(model, X_cleaned, y_cleaned)
     print(f"{model_name} - Accuracy: {accuracy:.2%}, Precision: {precision:.2%}, Recall: {recall:.2%}")
 
 print("Saving trained models and vectorizer...")
